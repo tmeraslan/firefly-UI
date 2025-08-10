ada1837cc7261ca5a8a05007141fc2055c32dab6
tamer <tmeraslan1@gmail.com>
2025-08-10 20:44:32 +0300

    fix

--- tests/test_sign_in.py
+++ tests/test_sign_in.py
@@ -9,7 +9,7 @@ from config import EMAIL, PASSWORD
 class TestSignInFlow(unittest.TestCase):
     def setUp(self):
         options = Options()
-        # options.add_argument("--headless=new")
+        options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
