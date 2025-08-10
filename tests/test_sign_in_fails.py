671fccde9003ec6ec55c3aff7d0670ec451b8409
tamer <tmeraslan1@gmail.com>
2025-08-10 20:58:15 +0300

    signfails

--- tests/test_sign_in_fails.py
+++ tests/test_sign_in_fails.py
@@ -1,33 +1,49 @@
-# import pytest
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-# from pages.sign_in_page import SignInPage
-# import time
-# from config import INVALID_EMAIL, INVALID_PASSWORD
-# from config import EMAIL, PASSWORD
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from pages.sign_in_page import SignInPage
+import time
+from config import INVALID_EMAIL, INVALID_PASSWORD
+from config import EMAIL, PASSWORD
+from selenium.webdriver.chrome.options import Options
+import tempfile
+import shutil
 
 
-# # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-# @pytest.fixture
-# def driver():
-#     driver = webdriver.Chrome()
-#     driver.maximize_window()
-#     yield driver
-#     driver.quit()
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
 
-# def test_login_fails_with_invalid_email(driver):
-#     page = SignInPage(driver)
-#     page.load()
-#     page.login(INVALID_EMAIL, PASSWORD, remember=False)
-#     # Wait for error message or failed login indication
-#     assert page.is_invalid_credentials_alert_present()
-#     time.sleep(2)
+    # צור תיקיית פרופיל זמנית ל-user data
+    user_data_dir = tempfile.mkdtemp(prefix="chrome-user-data-")
+    options.add_argument(f"--user-data-dir={user_data_dir}")
 
-# def test_login_fails_with_invalid_password(driver):
-#     page = SignInPage(driver)
-#     page.load()
-#     page.login(EMAIL, INVALID_PASSWORD, remember=False)
-#     # Wait for error message or failed login indication
-#     assert page.is_invalid_credentials_alert_present()
-#     time.sleep(2)
+    driver = webdriver.Chrome(options=options)
+    options.add_argument("window-size=1920,1080")
+    yield driver
+
+    driver.quit()
+
+    # מחק את תיקיית הפרופיל אחרי סגירת הדרייבר
+    shutil.rmtree(user_data_dir, ignore_errors=True)
+
+def test_login_fails_with_invalid_email(driver):
+    page = SignInPage(driver)
+    page.load()
+    page.login(INVALID_EMAIL, PASSWORD, remember=False)
+    # Wait for error message or failed login indication
+    assert page.is_invalid_credentials_alert_present()
+    time.sleep(2)
+
+def test_login_fails_with_invalid_password(driver):
+    page = SignInPage(driver)
+    page.load()
+    page.login(EMAIL, INVALID_PASSWORD, remember=False)
+    # Wait for error message or failed login indication
+    assert page.is_invalid_credentials_alert_present()
+    time.sleep(2)
