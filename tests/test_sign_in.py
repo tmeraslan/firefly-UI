80fe29aefbea4fd2459eda6eda8e0dc855812960
tamer <tmeraslan1@gmail.com>
2025-08-10 20:08:56 +0300

    fixHeadless

--- tests/test_sign_in.py
+++ tests/test_sign_in.py
@@ -14,7 +14,7 @@ import shutil
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--headless=new")
+    # options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
