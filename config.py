7260557f7925c550633dc2c641a78d9601164fb0
tmeraslan <tmeraslan1@gmail.com>
2025-09-29 14:27:02 +0300

    config

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 
 #config.py
 # URL = "http://127.0.0.1:8082/"
-URL = "http://localhost:8080"
+# URL = "http://localhost:8080"
+URL = "http://34.245.191.147:8082"
 
 EMAIL = "admin3@example.com"
 PASSWORD = "asdqwezxc123456!"
